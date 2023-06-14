10ea051f29a0853d767ac6952a259ed184e342da
lanre2nite <olabagy@hotmail.com>
2023-06-13 21:58:02 -0700

    Updated Task

---
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+exports.addMeMaybe = function addMeMaybe (number, theFunction) {
+  return theFunction(++number);
+};#!/usr/bin/node
+exports.addMeMaybe = function addMeMaybe (number, theFunction) {
+  return theFunction(++number);
+};
